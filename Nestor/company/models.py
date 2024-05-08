0324c7bc9f2b21593e1c3946f00f39ac1b4c57bc
GiantMushy <thorvardur23@ru.is>
2024-05-08 14:46:54 +0000

    fix Company Zipcode

--- Nestor/company/models.py
+++ Nestor/company/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from user.models import Profile 
+from common.models import ZipCode
 
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
+    zipcode = models.ForeignKey(ZipCode, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=100)
     number_of_employees = models.IntegerField()
     description = models.TextField(max_length=1000)
     link = models.URLField(max_length=1000)
     logo = models.CharField(max_length=9999)
-    description = models.CharField(max_length=9999)
     phone = models.CharField(max_length=12)
     email = models.CharField(max_length=50)
 
@@ -18,8 +19,8 @@ class Company(models.Model):
 
 
 class Employees(models.Model):
-    user =  models.ForeignKey(Profile, on_delete=models.CASCADE)
-    company =  models.ForeignKey(Company, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.name + " " + self.company.name
