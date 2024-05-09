b86f868c9ba3da61449518cb597f995db71544bf
Sævar Reynisson <saevarr99@gmail.com>
2024-05-09 12:01:03 +0000

    job data linked

--- Nestor/job/views.py
+++ Nestor/job/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from job.forms.job_form import JobCreateForm
 from job.models import Job
+from datetime import timedelta
 
 
 # Create your views here.
 
-jobs = [
-    { 'title': 'Software Engineer', 'company': 'Marel', 'percentage': '100', 'days': '12'},
-    { 'title': 'Bank Engineer', 'company': 'Arion', 'percentage': '90', 'days': '123'},
-    { 'title': 'Cunt Engineer', 'company': 'Islandsbanki', 'percentage': '25', 'days': '1'}
-]
-
+# jobs = [
+#     { 'title': 'Software Engineer', 'company': 'Marel', 'percentage': '100', 'days': '12'},
+#     { 'title': 'Bank Engineer', 'company': 'Arion', 'percentage': '90', 'days': '123'},
+#     { 'title': 'Cunt Engineer', 'company': 'Islandsbanki', 'percentage': '25', 'days': '1'}
+# ]
 
 def index(request):
-    return render(request, 'job/index.html', context={'jobs': jobs})
-
+    context = {
+        'job_data': Job.objects.all(),
+    }
+    return render(request, 'job/index.html', context={'jobs': Job.objects.all()})
 
 def get_job_by_id(request, id):
     return render(request, 'job/job_page.html', {
