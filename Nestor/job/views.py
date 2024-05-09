b4a24298e1186176f0c1b075f719152bf31f68b1
GiantMushy <thorvardur23@ru.is>
2024-05-09 14:31:25 +0000

    Create_job POST

--- Nestor/job/views.py
+++ Nestor/job/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from job.forms.job_form import JobCreateForm
 from job.models import Job
 
 
 # Create your views here.
 
-jobs = [
-    { 'title': 'Software Engineer', 'company': 'Marel', 'percentage': '100', 'days': '12'},
-    { 'title': 'Bank Engineer', 'company': 'Arion', 'percentage': '90', 'days': '123'},
-    { 'title': 'Cunt Engineer', 'company': 'Islandsbanki', 'percentage': '25', 'days': '1'}
-]
+#jobs = [
+#    { 'name': 'Software Engineer', 'company.name': 'Marel', 'percentage': '100', 'application_due_date': '12'},
+#    { 'name': 'Bank Engineer', 'company.name': 'Arion', 'percentage': '90', 'application_due_date': '123'},
+#    { 'name': 'Cunt Engineer', 'company.name': 'Islandsbanki', 'percentage': '25', 'application_due_date': '1'}
+#]
 
 
 def index(request):
-    return render(request, 'job/index.html', context={'jobs': jobs})
+    context = {'jobs': Job.objects.all().order_by('name')}
+    return render(request, 'job/index.html', context)
 
 
 def get_job_by_id(request, id):
@@ -24,7 +25,10 @@ def get_job_by_id(request, id):
 
 def create_job(request):
     if request.method == 'POST':
-        print(1)
+        form = JobCreateForm(data=request.POST)
+        if form.is_valid():
+            job = form.save()
+            return redirect('job_index')
     else:
         form = JobCreateForm()
     return render(request, 'job/create_job.html', {
