a6d72d16fca3e5923434e0fe23422c31f38a8bdf
GiantMushy <thorvardur23@ru.is>
2024-05-10 20:22:02 +0000

    edit_CV unfinished

--- Nestor/applicant/views.py
+++ Nestor/applicant/views.py
@@ -1,30 +1,83 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from applicant.models import Applicant, CVEducation, CVExperience, CVReferences
-from applicant.forms.applicant_form import ApplicantForm
+from applicant.forms.applicant_form import *
 from common.models import ZipCode
 
 
 def applicant(request):
     applicant = Applicant.objects.filter(user=request.user).first()
-    if request.method == 'POST':
-        form = ApplicantForm(instance=applicant, data=request.POST)
-        if form.is_valid():
-            applicant = form.save(commit=False)
-            applicant.user = request.user
-            applicant.save()
-            return redirect('applicant')
-    experience = CVExperience.objects.filter(applicant=applicant).all()
-    education = CVEducation.objects.filter(applicant=applicant).all()
+    experiences = CVExperience.objects.filter(applicant=applicant).all()
+    educations = CVEducation.objects.filter(applicant=applicant).all()
     references = CVReferences.objects.filter(applicant=applicant).all()
-    return render(request, 'applicant/applicant.html', {
-        'form': ApplicantForm(instance=applicant),
-        'applicant': applicant,
-        'zip_options': ZipCode.objects.all(),
-        'experiences': experience,
-        'educations': education,
-        'references': references
-    })
+
+    if request.method == 'POST':
+        applicant_form = ApplicantForm(instance=applicant, data=request.POST)
+        validity = True
+
+        if applicant_form.is_valid():
+            applicant = applicant_form.save(commit=False)
+            applicant.user = request.user
+        else:
+            validity = False
+
+        for experience in experiences:
+            experience_form = ExperienceForm(instance=experience, data=request.POST)
+            if experience_form.is_valid():
+                experience = experience_form.save(commit=False)
+                experience.user = request.user
+            else:
+                validity = False
+
+        for education in educations:
+            education_form = EducationForm(instance=education, data=request.POST)
+            if education_form.is_valid():
+                education = education_form.save(commit=False)
+                education.user = request.user
+            else:
+                validity = False
+
+        for reference in references:
+            reference_form = ReferenceForm(instance=reference, data=request.POST)
+            if reference_form.is_valid():
+                reference = reference_form.save(commit=False)
+                reference.user = request.user
+            else:
+                validity = False
+
+        if validity:
+            applicant.save()
+            for experience in experiences:
+                experience.save()
+            for education in educations:
+                education.save()
+            for reference in references:
+                reference.save()
+            return redirect('applicant')
+
+        experience_forms = {}
+        for experience in experiences:
+            experience_forms[experience]: ExperienceForm(instance=experience)
+        education_forms = {}
+        for education in educations:
+            education_forms[education]: EducationForm(instance=education)
+        reference_forms = {}
+        for reference in references:
+            reference_forms[reference]: ReferenceForm(instance=reference)
+
+        context = {
+            'applicant_form': ApplicantForm(instance=applicant),
+            'experience_forms': experience_forms,
+            'education_forms': education_forms,
+            'reference_forms': reference_forms,
+            'applicant': applicant,
+            'zip_options': ZipCode.objects.all(),
+            'experiences': experiences,
+            'educations': educations,
+            'references': references
+        }
+
+    return render(request, 'applicant/applicant.html', context)
 
 
 def index(request):
