1c8daae6b6f45d946b75ff504d55a9098a44b089
GiantMushy <thorvardur23@ru.is>
2024-05-10 13:10:06 +0000

    Edit contact-info incomplete

--- Nestor/applicant/views.py
+++ Nestor/applicant/views.py
@@ -5,10 +5,6 @@ from applicant.forms.applicant_form import ApplicantForm
 from common.models import ZipCode
 
 
-
-# Create your views here.
-
-
 def applicant(request):
     applicant = Applicant.objects.filter(user=request.user).first()
     if request.method == 'POST':
@@ -20,14 +16,11 @@ def applicant(request):
             return redirect('applicant')
     return render(request, 'applicant/applicant.html', {
         'form': ApplicantForm(instance=applicant),
-        'applicant': applicant
+        'applicant': applicant,
+        'zip_options': ZipCode.objects.all()
     })
+
 
 def index(request):
     applicant = Applicant.objects.filter(user=request.user).first()
     return render(request,'applicant/index.html', {'applicant': applicant})
-
-
-def zipcode_dropdown(request):
-    zip_options = ZipCode.objects.all()
-    return render(request, 'applicant/applicant.html', {'zip_options': zip_options})
--- Nestor/static/js/profile.js
+++ Nestor/static/js/profile.js
@@ -5,3 +5,9 @@ const toggleEditMode = () => {
 	edit.classList.remove("hidden");
 }
 
+const toggleEditContactInfo = () => {
+	const view = document.getElementById("contact-info-view");
+	view.classList.add("hidden");
+	const edit = document.getElementById("contact-info-edit");
+	edit.classList.remove("hidden");
+}
