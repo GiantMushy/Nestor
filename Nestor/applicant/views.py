c74d0cbf839ee52ea9ada86b09f22e3b563efbc5
GiantMushy <thorvardur23@ru.is>
2024-05-11 19:29:14 +0000

    Add CV fields functional

--- Nestor/applicant/views.py
+++ Nestor/applicant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
-from applicant.models import Applicant, CVEducation, CVExperience, CVReferences
+from applicant.models import *
 from applicant.forms.applicant_form import *
 from common.models import ZipCode
 
@@ -12,60 +12,73 @@ def applicant(request):
     educations = CVEducation.objects.filter(applicant=applicant).all()
     references = CVReferences.objects.filter(applicant=applicant).all()
 
-
     if request.method == 'POST':
+        print("request:")
         print(request)
-        applicant_form = ApplicantForm(instance=applicant, data=request.POST)
+        print("request.POST:")
+        print(request.POST)
         validity = True
 
-        if applicant_form.is_valid():
-            applicant = applicant_form.save(commit=False)
-            applicant.user = request.user
-            applicant.save()
-            print("Contact Info Validity Confirmed")
-        else:
-            validity = False
-
-#        for experience in experiences:
-#            experience_form = ExperienceForm(instance=experience, data=request.POST)
-#            if experience_form.is_valid():
-#                experience = experience_form.save(commit=False)
-#                experience.user = request.user
-#                experience.save()
-#                print("Experience Validity Confirmed")
-#            else:
-#                print("Experience Validity Failed")
-#                validity = False
-
-#        for education in educations:
-#            education_form = EducationForm(instance=education, data=request.POST)
-#            if education_form.is_valid():
-#                print("Education Validity Confirmed")
-#                education = education_form.save(commit=False)
-#                education.user = request.user
-#            else:
-#                validity = False
-
-        for reference in references:
-            reference_form = ReferenceForm(instance=reference, data=request.POST)
-            print(reference_form)
-            print(reference)
-            if reference_form.is_valid():
-                print("Reference Validity Confirmed")
-                reference = reference_form.save(commit=False)
-                reference.user = request.user
+        if "zipcode" in request.POST:  #Applicant
+            applicant_form = ApplicantForm(instance=applicant, data=request.POST)
+            if applicant_form.is_valid():
+                applicant = applicant_form.save(commit=False)
+                applicant.user = request.user
+                applicant.save()
+                print("Contact Info Validity Confirmed")
             else:
-                print("Reference Validity Failed")
+                validity = False
+
+        if "workplace_name" in request.POST and "start_date" in request.POST:  #Experience
+            experience_form = ExperienceForm(data=request.POST)
+            print(experience_form.errors)
+            if experience_form.is_valid():
+                print("Experience Validity SUCCESS")
+                experience = experience_form.save(commit=False)
+                applicant = Applicant.objects.filter(user=request.user).first()
+                experience.save()
+                cvexp = CVExperience(applicant=applicant, experience=experience)
+                cvexp.save()
+
+            else:
+                print("Experience Validity Failed: ")
+                print(experience_form.errors)
+                validity = False
+
+        if "school_name" in request.POST:  #Education
+            education_form = EducationForm(data=request.POST)
+            print(education_form.errors)
+            if education_form.is_valid():
+                print("Education Validity SUCCESS")
+                education = education_form.save(commit=False)
+                applicant = Applicant.objects.filter(user=request.user).first()
+                education.save()
+                cvedu = CVEducation(applicant=applicant, education=education)
+                cvedu.save()
+
+            else:
+                print("Education Validity Failed: ")
+                print(education_form.errors)
+                validity = False
+
+        if "is_contactable" in request.POST:  #References
+            reference_form = ReferenceForm(data=request.POST)
+            print(reference_form.errors)
+            if reference_form.is_valid():
+                print("Reference Validity SUCCESS")
+                reference = reference_form.save(commit=False)
+                applicant = Applicant.objects.filter(user=request.user).first()
+                reference.save()
+                cvref = CVReferences(applicant=applicant, reference=reference)
+                cvref.save()
+
+            else:
+                print("Reference Validity Failed: ")
+                print(reference_form.errors)
                 validity = False
 
         if validity:
             print("Validity Confirmed")
-#            for experience in experiences:
-#                experience.save()
-#            for education in educations:
-#                education.save()
-#            for reference in references:
-#                reference.save()
             return redirect('applicant')
         else:
             print("Validity Failed")
@@ -74,6 +87,7 @@ def applicant(request):
     context = {
         'applicant': applicant,
         'zip_options': ZipCode.objects.all(),
+        'education_levels': EducationLevel.objects.all(),
         'experiences': experiences,
         'educations': educations,
         'references': references
@@ -83,4 +97,4 @@ def applicant(request):
 
 def index(request):
     applicant = Applicant.objects.filter(user=request.user).first()
-    return render(request,'applicant/index.html', {'applicant': applicant})
+    return render(request, 'applicant/index.html', {'applicant': applicant})
