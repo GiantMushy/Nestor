633800565444a1ea952ccfc86cd1df083efa6864
GiantMushy <thorvardur23@ru.is>
2024-05-11 02:15:27 +0000

    unfinished stuff

--- Nestor/applicant/views.py
+++ Nestor/applicant/views.py
@@ -6,77 +6,78 @@ from common.models import ZipCode
 
 
 def applicant(request):
+    print("Hello what")
     applicant = Applicant.objects.filter(user=request.user).first()
     experiences = CVExperience.objects.filter(applicant=applicant).all()
     educations = CVEducation.objects.filter(applicant=applicant).all()
     references = CVReferences.objects.filter(applicant=applicant).all()
 
+
     if request.method == 'POST':
+        print(request)
         applicant_form = ApplicantForm(instance=applicant, data=request.POST)
         validity = True
 
         if applicant_form.is_valid():
             applicant = applicant_form.save(commit=False)
             applicant.user = request.user
+            applicant.save()
+            print("Contact Info Validity Confirmed")
         else:
             validity = False
 
-        for experience in experiences:
-            experience_form = ExperienceForm(instance=experience, data=request.POST)
-            if experience_form.is_valid():
-                experience = experience_form.save(commit=False)
-                experience.user = request.user
-            else:
-                validity = False
+#        for experience in experiences:
+#            experience_form = ExperienceForm(instance=experience, data=request.POST)
+#            if experience_form.is_valid():
+#                experience = experience_form.save(commit=False)
+#                experience.user = request.user
+#                experience.save()
+#                print("Experience Validity Confirmed")
+#            else:
+#                print("Experience Validity Failed")
+#                validity = False
 
-        for education in educations:
-            education_form = EducationForm(instance=education, data=request.POST)
-            if education_form.is_valid():
-                education = education_form.save(commit=False)
-                education.user = request.user
-            else:
-                validity = False
+#        for education in educations:
+#            education_form = EducationForm(instance=education, data=request.POST)
+#            if education_form.is_valid():
+#                print("Education Validity Confirmed")
+#                education = education_form.save(commit=False)
+#                education.user = request.user
+#            else:
+#                validity = False
 
         for reference in references:
             reference_form = ReferenceForm(instance=reference, data=request.POST)
+            print(reference_form)
+            print(reference)
             if reference_form.is_valid():
+                print("Reference Validity Confirmed")
                 reference = reference_form.save(commit=False)
                 reference.user = request.user
             else:
+                print("Reference Validity Failed")
                 validity = False
 
         if validity:
-            applicant.save()
-            for experience in experiences:
-                experience.save()
-            for education in educations:
-                education.save()
-            for reference in references:
-                reference.save()
+            print("Validity Confirmed")
+#            for experience in experiences:
+#                experience.save()
+#            for education in educations:
+#                education.save()
+#            for reference in references:
+#                reference.save()
+            return redirect('applicant')
+        else:
+            print("Validity Failed")
             return redirect('applicant')
 
-        experience_forms = {}
-        for experience in experiences:
-            experience_forms[experience]: ExperienceForm(instance=experience)
-        education_forms = {}
-        for education in educations:
-            education_forms[education]: EducationForm(instance=education)
-        reference_forms = {}
-        for reference in references:
-            reference_forms[reference]: ReferenceForm(instance=reference)
-
-        context = {
-            'applicant_form': ApplicantForm(instance=applicant),
-            'experience_forms': experience_forms,
-            'education_forms': education_forms,
-            'reference_forms': reference_forms,
-            'applicant': applicant,
-            'zip_options': ZipCode.objects.all(),
-            'experiences': experiences,
-            'educations': educations,
-            'references': references
-        }
-
+    context = {
+        'applicant': applicant,
+        'zip_options': ZipCode.objects.all(),
+        'experiences': experiences,
+        'educations': educations,
+        'references': references
+    }
     return render(request, 'applicant/applicant.html', context)
 
 
--- Nestor/static/js/profile.js
+++ Nestor/static/js/profile.js
@@ -12,16 +12,36 @@ const toggleEditExperience = () => {
 	edit.classList.remove("hidden");
 }
 
+const toggleCreateExperience = () => {
+	const view = document.getElementById("experience-new");
+	if (view.classList.contains("hidden")) {view.classList.remove("hidden")}
+	else {view.classList.remove("hidden")}
+
+
+}
+
+
 const toggleEditEducation = () => {
 	const view = document.getElementById("education-view");
 	view.classList.add("hidden");
 	const edit = document.getElementById("education-edit");
 	edit.classList.remove("hidden");
 }
+const toggleCreateEducation = () => {
+	const view = document.getElementById("education-new");
+	if (view.classList.contains("hidden")) {view.classList.remove("hidden")}
+	else {view.classList.remove("hidden")}
+}
+
 
 const toggleEditReference = () => {
 	const view = document.getElementById("reference-view");
 	view.classList.add("hidden");
 	const edit = document.getElementById("reference-edit");
 	edit.classList.remove("hidden");
+}
+const toggleCreateReference = () => {
+	const view = document.getElementById("reference-new");
+	if (view.classList.contains("hidden")) {view.classList.remove("hidden")}
+	else {view.classList.remove("hidden")}
 }
