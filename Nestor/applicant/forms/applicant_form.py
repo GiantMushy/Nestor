1ba8cef84d8f3b8be48d95fc9b4822106e6cefc5
buxnabenni <jonsson.isak99@gmail.com>
2024-05-12 14:10:33 +0000

    da

--- Nestor/applicant/forms/applicant_form.py
+++ Nestor/applicant/forms/applicant_form.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, widgets
-from applicant.models import Applicant
+from applicant.models import Applicant, Experience, Education, References
+
 
 class ApplicantForm(ModelForm):
     class Meta:
@@ -9,3 +10,43 @@ class ApplicantForm(ModelForm):
                 'profile_image': widgets.TextInput(attrs={'class': 'form-control'})
         } 
 
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        exclude = ['id']
+        widgets = {
+            'workplace_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'role': widgets.TextInput(attrs={'class': 'form-control'}),
+            'start_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'end_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'description': widgets.Textarea(attrs={'class': 'form-control'})
+        }
+
+
+class EducationForm(ModelForm):
+    class Meta:
+        model = Education
+        exclude = ['id']
+        widgets = {
+            'school_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'degree': widgets.TextInput(attrs={'class': 'form-control'}),
+            'level': widgets.TextInput(attrs={'class': 'form-control'}),
+            'start_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'end_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'description': widgets.Textarea(attrs={'class': 'form-control'})
+        }
+
+
+class ReferenceForm(ModelForm):
+    class Meta:
+        model = References
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.EmailInput(attrs={'class': 'form-control'}),
+            'phone': widgets.TextInput(attrs={'class': 'form-control'}),
+            'workplace_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'role': widgets.TextInput(attrs={'class': 'form-control'}),
+            'is_contactable': widgets.CheckboxInput(attrs={'class': 'checkbox'})
+        }
--- Nestor/static/js/profile.js
+++ Nestor/static/js/profile.js
@@ -1,13 +1,27 @@
-const toggleEditMode = () => {
-	const view = document.getElementById("view-profile");
-	view.classList.add("hidden");
-	const edit = document.getElementById("edit-profile");
-	edit.classList.remove("hidden");
-}
-
 const toggleEditContactInfo = () => {
 	const view = document.getElementById("contact-info-view");
 	view.classList.add("hidden");
 	const edit = document.getElementById("contact-info-edit");
+	edit.classList.remove("hidden");
+}
+
+const toggleEditExperience = () => {
+	const view = document.getElementById("experience-view");
+	view.classList.add("hidden");
+	const edit = document.getElementById("experience-edit");
+	edit.classList.remove("hidden");
+}
+
+const toggleEditEducation = () => {
+	const view = document.getElementById("education-view");
+	view.classList.add("hidden");
+	const edit = document.getElementById("education-edit");
+	edit.classList.remove("hidden");
+}
+
+const toggleEditReference = () => {
+	const view = document.getElementById("reference-view");
+	view.classList.add("hidden");
+	const edit = document.getElementById("reference-edit");
 	edit.classList.remove("hidden");
 }
