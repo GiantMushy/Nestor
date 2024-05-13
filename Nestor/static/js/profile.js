ca6480bceb2e011eca1110e78712de33106b7207
GiantMushy <thorvardur23@ru.is>
2024-05-13 14:54:24 +0000

    Edit CV data finished

--- Nestor/static/js/profile.js
+++ Nestor/static/js/profile.js
@@ -1,45 +1,45 @@
 const toggleEditContactInfo = () => {
-	const view = document.getElementById("contact-info-view");
+	const view = document.getElementById("contact-info/view");
 	view.classList.add("hidden");
-	const edit = document.getElementById("contact-info-edit");
+	const edit = document.getElementById("contact-info/edit");
 	edit.classList.remove("hidden");
 }
 
 
-const toggleEditExperience = () => {
-	const view = document.getElementById("experience-view");
+const toggleEditExperience = (id) => {
+	const view = document.getElementById(`experience/view/${id}`);
 	view.classList.add("hidden");
-	const edit = document.getElementById("experience-edit");
+	const edit = document.getElementById(`experience/edit/${id}`);
 	edit.classList.remove("hidden");
 }
 const toggleAddExperience = () => {
-	const view = document.getElementById("experience-new");
+	const view = document.getElementById("experience/new");
 	if (view.classList.contains("hidden")) { view.classList.remove("hidden") }
 	else { view.classList.add("hidden") }
 }
 
 
-const toggleEditEducation = () => {
-	const view = document.getElementById("education-view");
+const toggleEditEducation = (id) => {
+	const view = document.getElementById(`education/view/${id}`);
 	view.classList.add("hidden");
-	const edit = document.getElementById("education-edit");
+	const edit = document.getElementById(`education/edit/${id}`);
 	edit.classList.remove("hidden");
 }
 const toggleAddEducation = () => {
-	const view = document.getElementById("education-new");
+	const view = document.getElementById("education/new");
 	if (view.classList.contains("hidden")) { view.classList.remove("hidden") }
 	else { view.classList.add("hidden") }
 }
 
 
-const toggleEditReference = () => {
-	const view = document.getElementById("reference-view");
+const toggleEditReference = (id) => {
+	const view = document.getElementById(`reference/view/${id}`);
 	view.classList.add("hidden");
-	const edit = document.getElementById("reference-edit");
+	const edit = document.getElementById(`reference/edit/${id}`);
 	edit.classList.remove("hidden");
 }
 const toggleAddReference = () => {
-	const view = document.getElementById("reference-new");
+	const view = document.getElementById("reference/new");
 	if (view.classList.contains("hidden")) { view.classList.remove("hidden") }
 	else { view.classList.add("hidden") }
 }
