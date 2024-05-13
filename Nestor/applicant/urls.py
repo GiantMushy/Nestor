5ccaceb2a3d27650942fef352db8aa9bf2c252bd
GiantMushy <thorvardur23@ru.is>
2024-05-13 10:45:26 +0000

    BugFix Add CV data

--- Nestor/applicant/urls.py
+++ Nestor/applicant/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='applicant_index'),
-    path('applicant', views.applicant, name='applicant')
+    path('applicant', views.applicant, name='applicant'),
+    path('applicant/contact-info', views.contact_info, name='applicant/contact-info'),
+    path('applicant/experience_add', views.experience_add, name='applicant/experience_add'),
+    path('applicant/education_add', views.education_add, name='applicant/education_add'),
+    path('applicant/reference_add', views.reference_add, name='applicant/reference_add'),
+    path('applicant/experience_edit', views.experience_edit, name='applicant/experience_edit'),
+    path('applicant/education_edit', views.education_edit, name='applicant/education_edit'),
+    path('applicant/reference_edit', views.reference_edit, name='applicant/reference_edit')
 ]
