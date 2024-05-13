6eea4741830a2bc6211ca6a3326f41e7d37b2f22
GiantMushy <thorvardur23@ru.is>
2024-05-13 14:12:32 +0000

    CV editing incomplete

--- Nestor/applicant/views.py
+++ Nestor/applicant/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 # from django.contrib.auth.forms import UserCreationForm
 from applicant.models import *
 from applicant.forms.applicant_form import *
@@ -22,6 +22,7 @@ def contact_info(request):
 
 
 def experience_add(request):
+    print("Adding Experience")
     applicant = Applicant.objects.filter(user=request.user).first()
 
     experience_form = ExperienceForm(data=request.POST)
@@ -39,10 +40,24 @@ def experience_add(request):
 
 
 def experience_edit(request):
-    return redirect('applicant')
+    print("Editing Experience")
+    applicant = Applicant.objects.filter(user=request.user).first()
+    experience_id = request.POST.get('experience_id')
+    experience = get_object_or_404(Experience, id=experience_id)
+    experience_form = ExperienceForm(data=request.POST, instance=experience)
+
+    if experience_form.is_valid():
+        print("POST Experience Validity SUCCESS")
+        experience_form.save()
+        return redirect('applicant')
+    else:
+        print("POST Experience Validity Failed")
+        return redirect('applicant')
+
 
 
 def education_add(request):
+    print("Adding Education")
     applicant = Applicant.objects.filter(user=request.user).first()
 
     education_form = EducationForm(data=request.POST)
@@ -60,10 +75,23 @@ def education_add(request):
 
 
 def education_edit(request):
-    return redirect('applicant')
+    print("Editing Education")
+    applicant = Applicant.objects.filter(user=request.user).first()
+    education_id = request.POST.get('education_id')
+    education = get_object_or_404(Education, id=education_id)
+    education_form = EducationForm(data=request.POST, instance=education)
+
+    if education_form.is_valid():
+        print("POST Education Validity SUCCESS")
+        education_form.save()
+        return redirect('applicant')
+    else:
+        print("POST Education Validity Failed")
+        return redirect('applicant')
 
 
 def reference_add(request):
+    print("Adding Reference")
     applicant = Applicant.objects.filter(user=request.user).first()
 
     reference_form = ReferenceForm(data=request.POST)
@@ -81,27 +109,23 @@ def reference_add(request):
 
 
 def reference_edit(request):
-    return redirect('applicant')
+    print("Editing Reference")
+    applicant = Applicant.objects.filter(user=request.user).first()
+    reference_id = request.POST.get('reference_id')
+    reference = get_object_or_404(References, id=reference_id)
+    reference_form = ReferenceForm(data=request.POST, instance=reference)
+
+    if reference_form.is_valid():
+        print("POST Reference Validity SUCCESS")
+        reference_form.save()
+        return redirect('applicant')
+    else:
+        print("POST Reference Validity Failed")
+        return redirect('applicant')
 
 
 def applicant(request):
-    applicant = Applicant.objects.filter(user=request.user).first()
-    experiences = CVExperience.objects.filter(applicant=applicant).all()
-    educations = CVEducation.objects.filter(applicant=applicant).all()
-    references = CVReferences.objects.filter(applicant=applicant).all()
-
-    context = {
-        'applicant': applicant,
-        'zip_options': ZipCode.objects.all(),
-        'education_levels': EducationLevel.objects.all(),
-        'experiences': experiences,
-        'educations': educations,
-        'references': references
-    }
-    return render(request, 'applicant/applicant.html', context)
-
-
-def render_data(request):
+    print("Displaying Applicant Data")
     applicant = Applicant.objects.filter(user=request.user).first()
     experiences = CVExperience.objects.filter(applicant=applicant).all()
     educations = CVEducation.objects.filter(applicant=applicant).all()
