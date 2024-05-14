77468b79fd2c34fada57350f883d6aa10ec93c40
arnar jónsson <107851212+arnarjons99@users.noreply.github.com>
2024-05-14 12:55:21 +0000

    finishing company-button
    blabal

--- Nestor/company/views.py
+++ Nestor/company/views.py
@@ -3,7 +3,7 @@ from company.forms.company_form import CompanyCreateForm
 from company.models import Company, Employee
 from common.models import JobCategory, City
 from django.utils import timezone
-from job.models import Job
+from job.models import Job, FavoriteJob
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
@@ -37,12 +37,14 @@ def index(request):
 def get_company_by_id(request, id):
     active_section = get_active_section(request)
     all_jobs = Job.objects.filter(company_id=id).order_by('name')
+    fav_jobs = FavoriteJob.objects.filter(applicant__user_id=request.user.id).all()
 
     jobs = [add_days_left(job) for job in all_jobs]
     filtered_jobs = [job for job in jobs if int(job.days_left) > 0]
     return render(request, 'company/company_page.html', {
         'company': get_object_or_404(Company, pk=id),
         'jobs': filtered_jobs,
+        'fav_jobs': fav_jobs,
         'active_section': active_section
     })
 
