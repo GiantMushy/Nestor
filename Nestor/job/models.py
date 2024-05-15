9ce13707a3438bbf11d0b2fa79a06b649767642d
GiantMushy <thorvardur23@ru.is>
2024-05-15 19:11:18 +0000

    Update models.py

--- Nestor/job/models.py
+++ Nestor/job/models.py
@@ -36,7 +36,7 @@ class Application(models.Model):
     applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
-    cover_letter = models.TextField(max_length=1000)
+    cover_letter = models.TextField(max_length=1000, blank=True, null=True)
     is_submitted = models.BooleanField(default=False)
 
     def __str__(self):
