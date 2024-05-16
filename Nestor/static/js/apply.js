26108d89e1a4a4a49566d2870efaf5d92688e21f
buxnabenni <jonsson.isak99@gmail.com>
2024-05-16 14:27:47 +0000

    prufa + goTo

--- Nestor/static/js/apply.js
+++ Nestor/static/js/apply.js
@@ -1,29 +1,31 @@
 const goTo = (window) => {
-	if (typeof window === "object") {window = window.id}
+	if (typeof window === "object") { window = window.id }
 	const con = document.getElementById('cover-letter')
-	if (window == 'cover-letter') {if (con.classList.contains("hidden")) {con.classList.remove("hidden")}}
-	else {if (con.classList.contains("hidden")) {} else {con.classList.add("hidden")}}
+	if (window == 'cover-letter') { if (con.classList.contains("hidden")) { con.classList.remove("hidden") } }
+	else { if (con.classList.contains("hidden")) { } else { con.classList.add("hidden") } }
 
 	const exp = document.getElementById('experience')
-	if (window == 'experience') {if (exp.classList.contains("hidden")) {exp.classList.remove("hidden")}}
-	else {if (exp.classList.contains("hidden")) {} else {exp.classList.add("hidden")}}
+	if (window == 'experience') { if (exp.classList.contains("hidden")) { exp.classList.remove("hidden") } }
+	else { if (exp.classList.contains("hidden")) { } else { exp.classList.add("hidden") } }
 
 	const edu = document.getElementById('education')
-	if (window == 'education') {if (edu.classList.contains("hidden")) {edu.classList.remove("hidden")}}
-	else {if (edu.classList.contains("hidden")) {} else {edu.classList.add("hidden")}}
+	if (window == 'education') { if (edu.classList.contains("hidden")) { edu.classList.remove("hidden") } }
+	else { if (edu.classList.contains("hidden")) { } else { edu.classList.add("hidden") } }
 
 	const ref = document.getElementById('reference')
-	if (window == 'reference') {if (ref.classList.contains("hidden")) {ref.classList.remove("hidden")}}
-	else {if (ref.classList.contains("hidden")) {} else {ref.classList.add("hidden")}}
+	if (window == 'reference') { if (ref.classList.contains("hidden")) { ref.classList.remove("hidden") } }
+	else { if (ref.classList.contains("hidden")) { } else { ref.classList.add("hidden") } }
 
 	const skl = document.getElementById('skill')
-	if (window == 'skill') {if (skl.classList.contains("hidden")) {skl.classList.remove("hidden")}}
-	else {if (skl.classList.contains("hidden")) {} else {skl.classList.add("hidden")}}
+	if (window == 'skill') { if (skl.classList.contains("hidden")) { skl.classList.remove("hidden") } }
+	else { if (skl.classList.contains("hidden")) { } else { skl.classList.add("hidden") } }
 
 	const fnl = document.getElementById('final_review')
-	if (window == 'final_review') {if (fnl.classList.contains("hidden")) {fnl.classList.remove("hidden")}}
-	else {if (fnl.classList.contains("hidden")) {} else {fnl.classList.add("hidden")}}
+	if (window == 'final_review') { if (fnl.classList.contains("hidden")) { fnl.classList.remove("hidden") } }
+	else { if (fnl.classList.contains("hidden")) { } else { fnl.classList.add("hidden") } }
 }
+
+
 
 const toggleAddInstance = (element) => {
 	const view = document.getElementById(element + "/new");
